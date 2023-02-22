b7c14e6567c18ce188cbb010fc423c109a5d723f
tshepangm0723 <tshepangm0723@gmail.com>
2023-02-22 04:52:52 -0800

    hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * jack_bauer - Prints the minutes of a day
+ * Return: no return
+ */
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a <=2; a++)
+	{
+		for (b = 0; c <= 9; b++)
+		{
+			for (c = 0; c <= 8; c++)
+			{
+				for (d = 0; d <= 9; d++)
+				{
+					if (a >= 2 && b >= 4)
+						break;
+					_putchar(a + 48);
+					_putchar(b + 48);
+					_putchar(58);
+					_putchar(c + 48);
+					_putchar(d + 48);
+				}
+			}
+		}
+	}
+}
