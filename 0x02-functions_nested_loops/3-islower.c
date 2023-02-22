d781e4ca296de3c279a6f720dcdb874e3f13dd52
tshepangm0723 <tshepangm0723@gmail.com>
2023-02-22 02:39:02 -0800

    is lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,18 @@
 #include "main.h"
 /**
  * _islower - check if char is lowercase
- * @c: is the charto be checked
+ * @c: is the chart be checked
  * Return: 1 if char is lowercase, otherwise 0.
  */
 int _islower(int c)
 {
-	if (c <= 'a' && c <= 'z')
+	if (c >= 97 && c <= 122)
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
+	_putchar(\'n');
 }
