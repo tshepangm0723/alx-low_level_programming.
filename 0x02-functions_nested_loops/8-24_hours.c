511134aca98e015ed4bbe7657fa4546bba9c7456
tshepangm0723 <tshepangm0723@gmail.com>
2023-02-22 04:56:23 -0800

    hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,11 +7,11 @@ void jack_bauer(void)
 {
 	int a, b, c, d;
 
-	for (a = 0; a <=2; a++)
+	for (a = 0; a <= 2; a++)
 	{
 		for (b = 0; c <= 9; b++)
 		{
-			for (c = 0; c <= 8; c++)
+			for (c = 0; c <= 5; c++)
 			{
 				for (d = 0; d <= 9; d++)
 				{
