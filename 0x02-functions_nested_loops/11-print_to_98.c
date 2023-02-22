2283affa84b5f0f79fce1981327e005621d3c079
tshepangm0723 <tshepangm0723@gmail.com>
2023-02-22 10:02:36 -0800

    11 rint to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,26 +8,25 @@
  */
 void print_to_98(int n)
 {
-	int i' j;
+	int i, j;
 
 	if (n <= 98)
 	{
-		for (i = n;i <= 98; i++)
+		for (i = n; i <= 98; i++)
 		{
 			if (i != 98)
 				printf("%d\n", i);
 			else if (i == 98)
-				printf("%d\n"' i);
+				printf("%d\n", i);
 		}
-		else if (n >= 98)
+		} else if (n >= 98)
 		{
 			for (j = n; j >= 98; j--)
 			{
 				if (j != 98)
 					printf("%d\n", j);
-				els if (j == 98)
+				else if (j == 98)
 					printf("%d\n", j);
 			}
 		}
 	}
-}
