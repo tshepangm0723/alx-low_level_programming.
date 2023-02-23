de9fcdb9922ff6543ea933c44ab6d21eca96be73
tshepangm0723 <tshepangm0723@gmail.com>
2023-02-23 02:27:32 -0800

    is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,12 +2,17 @@
 /**
  * _isupper - uppercase letters
  * @c: char to check
- * Return: 0 or 1
+ * Return: 1 if c is uppercase, 0 otherwise.
  */
+
 int _isupper(int c)
 {
-	if (c >= 'A' && c <- 'Z')
-		return (1);
-	else
-		return (0);
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
