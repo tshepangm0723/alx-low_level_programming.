7cd297b3f32aa868b8f988e10ea7a78dc7d16133
tshepangm0723 <tshepangm0723@gmail.com>
2023-03-08 07:03:41 -0800

    strlen

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,7 +5,9 @@
  * Decription: finds length of a string
  * Return: string length
  */
+
 int _strlen_recursion(char *s);
+
 /**
  * _strlen_recursion - unction that returns the length of a string.
  * @s: string to be printed
@@ -19,8 +21,8 @@ int _strlen_recursion(char *s)
 
 	if (*s)
 	{
-		len++;
-		len += _strlen_recursion(s + 1);
+		len = _strlen_recursion(s + 1);
+		return (len = len + 1);
 	}
 	return (0);
 }
