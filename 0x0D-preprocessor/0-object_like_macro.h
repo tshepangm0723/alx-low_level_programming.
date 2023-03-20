ec7da74c36d6b7b32f2ba2f2ffa45d2833f2c84c
tshepangm0723 <tshepangm0723@gmail.com>
2023-03-20 11:31:38 -0700

    readme

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,11 +1,6 @@
 #ifndef OBJECT_LIKE_MACRO_H
 #define OBJECT_LIKE_MACRO_H
 
-/**
- * file: 0-object_like_macro.h
- * description: defines a macro named size of value 1024
- */
-
 #define SIZE 1024
 
 #endif
