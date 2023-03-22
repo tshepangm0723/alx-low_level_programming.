2835bbacac65417e5ced1d9a9925819a5767257e
tshepangm0723 <tshepangm0723@gmail.com>
2023-03-22 07:40:46 -0700

    print_name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,6 @@
 #include "function_pointers.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * print_name - function that prints a name.
  * @name: string to add
@@ -11,4 +12,5 @@ void print_name(char *name, void (*f)(char *))
 	if (name == NULL || f == NULL)
 		return;
 	f(name);
+
 }
