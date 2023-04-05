92db41959537b04f3dd9a9906ad263a99324966d
tshepangm0723 <tshepangm0723@gmail.com>
2023-04-05 15:56:48 -0700

    delete_nodeint

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -17,7 +17,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (index != 0)
 	{
-		for (j =0; j < index - 1 && last != NULL; j++)
+		for (j = 0; j < index - 1 && last != NULL; j++)
 		{
 			last = last->next;
 		}
