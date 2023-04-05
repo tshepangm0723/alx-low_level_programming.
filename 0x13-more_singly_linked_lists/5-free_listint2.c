10d55ae19d36221c51f75a41a43e4412a2847b8c
tshepangm0723 <tshepangm0723@gmail.com>
2023-04-05 14:26:24 -0700

    free_listin2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,15 +7,14 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp;
-	
+	listint_t *temp;	
 	if (head == NULL)
 		return;
 	while(*head != NULL)
 	{
 		temp = *head;
-		free(*head);
 		*head = temp->next;
+		free(*head);
 	}
 	*head = NULL;
 }
